fac5aa7657747f4e03107761d8327a98a3bc270f
Ashish Agarwal <ashishag07@gmail.com>
2024-12-28 20:12:16 +0530

    Provider from react-redux and redux store is applied to the app component.

--- src/App.js
+++ src/App.js
@@ -1,28 +1,32 @@
 import { useState } from "react";
+import { Provider } from "react-redux";
+
+import { store } from "./redux/store";
 import TodoForm from "./components/ToDoForm/ToDoForm";
 import TodoList from "./components/ToDoList/ToDoList";
 
-import './App.css';
+import "./App.css";
 
 function App() {
   const [todos, setTodos] = useState([]);
 
   const createTodo = (text) => {
-    setTodos([...todos, { id: todos.length + 1, text, completed: false}]);
+    setTodos([...todos, { id: todos.length + 1, text, completed: false }]);
   };
 
-  const toggleTodo = (index)=>{
+  const toggleTodo = (index) => {
     const list = [...todos];
     list[index].completed = !list[index].completed;
     setTodos(list);
-  }
+  };
 
   return (
     <div>
       <h1>To Do App</h1>
-
-      <TodoForm onCreateTodo={createTodo} />
-      <TodoList todos={todos} onToggle={toggleTodo} />
+      <Provider store={store}>
+        <TodoForm onCreateTodo={createTodo} />
+        <TodoList todos={todos} onToggle={toggleTodo} />
+      </Provider>
     </div>
   );
 }
