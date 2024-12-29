62fefa7a0b09ac720c76caf8b16909ae6fb8db15
Ashish Agarwal <ashishag07@gmail.com>
2024-12-29 18:07:38 +0530

    loggerMiddleware file is created
    configured redux store with logger middleware

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,10 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { todoReducer } from "./reducers/todoReducer";
 import { notificationReducer } from "./reducers/notificationReducer";
+import { loggerMiddleware } from "./middlewares/loggerMiddleware";
 
 export const store = configureStore({
   reducer: {
     todoReducer,
     notificationReducer,
   },
+  middleware: [loggerMiddleware],
 });
