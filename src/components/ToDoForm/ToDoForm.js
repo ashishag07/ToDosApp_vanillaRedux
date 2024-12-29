c20bfe14a2aa968cc797049aeb335194fce717e8
Ashish Agarwal <ashishag07@gmail.com>
2024-12-29 08:35:26 +0530

    Imported useDispatch from react-redux and Imported addTodo from redux/actions/todoAction
    Removed the props and fuction onCreateTodo. Instead we dispatch addTodo action

--- src/components/ToDoForm/ToDoForm.js
+++ src/components/ToDoForm/ToDoForm.js
@@ -1,27 +1,31 @@
 import { useState } from "react";
+import { useDispatch } from "react-redux";
+import { addTodo } from "../../redux/actions/todoAction";
 import "./ToDoForm.css";
 
-function ToDoForm({ onCreateTodo }) {
+function ToDoForm() {
   const [todoText, setTodoText] = useState("");
+  const dispatch = useDispatch();
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    onCreateTodo(todoText);
+    dispatch(addTodo(todoText));
     setTodoText("");
   };
 
   return (
     <div className="container">
-      
-    <form onSubmit={handleSubmit}>
-      <input
-        type="text"
-        className="form-control mb-3"
-        value={todoText}
-        onChange={(e) => setTodoText(e.target.value)}
-      />
-      <button className="btn btn-success float-end" type="submit">Create Todo</button>
-    </form>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          className="form-control mb-3"
+          value={todoText}
+          onChange={(e) => setTodoText(e.target.value)}
+        />
+        <button className="btn btn-success float-end" type="submit">
+          Create Todo
+        </button>
+      </form>
     </div>
   );
 }
