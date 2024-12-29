4f48f8887f42ecf22065481372ccde6dfb2d795e
Ashish Agarwal <ashishag07@gmail.com>
2024-12-29 13:42:42 +0530

    notification alert is added.

--- src/components/ToDoList/ToDoList.js
+++ src/components/ToDoList/ToDoList.js
@@ -1,13 +1,28 @@
 import "./ToDoList.css";
 import { useSelector, useDispatch } from "react-redux";
 import { todoActions, todoSelector } from "../../redux/reducers/todoReducer";
+import {
+  notificationActions,
+  notificationSelector,
+} from "../../redux/reducers/notificationReducer";
 
 function ToDoList() {
   const todos = useSelector(todoSelector);
+  const message = useSelector(notificationSelector);
   const dispatch = useDispatch();
 
+  if (message) {
+    setTimeout(() => {
+      dispatch(notificationActions.reset());
+    }, 2000);
+  }
   return (
     <div className="container">
+      {message && (
+        <div class="alert alert-success" role="alert">
+          {message}
+        </div>
+      )}
       <ul>
         {todos.map((todo, index) => (
           <li key={todo.id}>
