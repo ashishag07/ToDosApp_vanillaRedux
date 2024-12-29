2fa50b68cb8e178225bc755d23d74ce2a41cfad1
Ashish Agarwal <ashishag07@gmail.com>
2024-12-29 13:38:42 +0530

    store.js is configured along with the notification reducer.

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,8 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { todoReducer } from "./reducers/todoReducer";
+import { notificationReducer } from "./reducers/notificationReducer";
 
 export const store = configureStore({
   reducer: {
     todoReducer,
+    notificationReducer,
   },
 });
