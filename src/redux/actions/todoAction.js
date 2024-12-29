683a86c0510c29d691af443a6ff31d0ade688fa6
Ashish Agarwal <ashishag07@gmail.com>
2024-12-29 11:58:38 +0530

    todoAction.js file is deleted from redux/actions/todoActions.js

--- src/redux/actions/todoAction.js
+++
@@ -1,9 +0,0 @@
-// Step 1: create action constants
-
-export const ADD_TODO = "ADD_TODO";
-export const TOGGLE_TODO = "TOGGLE_TODO";
-
-// Step 2: action creators
-
-export const addTodo = (text) => ({ text, type: ADD_TODO });
-export const toggleTodo = (index) => ({ index, type: TOGGLE_TODO });
